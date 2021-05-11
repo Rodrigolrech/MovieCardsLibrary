682bf274b278b43424e0352eb0aff3124129b058
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-11 18:16:29 -0300

    Rating implemented at MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
@@ -11,7 +12,18 @@ export default class MovieCard extends Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+  }).isRequired,
 }
